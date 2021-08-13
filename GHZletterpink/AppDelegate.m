8405fe496e8129c027a046bc04c8ea263310d926
ghz <122928667@qq.com>
2021-08-13 11:59:54 +0800

    做了UI

--- GHZletterpink/AppDelegate.m
+++ GHZletterpink/AppDelegate.m
@@ -15,7 +15,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSLog(@"做了UI");
     return YES;
 }
 
